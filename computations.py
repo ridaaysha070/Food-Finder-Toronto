13d87803af9efc4a547460dd18903141b7bbb816
leimaya <163908110+leimaya@users.noreply.github.com>
2024-04-04 12:05:42 -0400

    Add files via upload

--- computations.py
+++ computations.py
@@ -2,16 +2,15 @@
 from __future__ import annotations
 from typing import Any, Optional
 import math
-import csv
-import requests
 from requests.exceptions import MissingSchema
 import plotly.express as px
 from geopy.geocoders import Nominatim
 import pandas as pd
+import requests
 
-RAW_DATA = pd.read_csv('trt_rest.csv')
+RAWDATA = pd.read_csv('trt_rest.csv')
 
-DATA = RAW_DATA.dropna().drop_duplicates(subset=['Restaurant Address', 'Category'], keep='first')
+DATA = RAWDATA.dropna().drop_duplicates(subset=['Restaurant Address', 'Category'], keep='first')
 
 RESTAURANT_QUESTIONS = [
     'What is your price range?\nUnder $10\n$11-30\n$31-60\nAbove $61',
@@ -31,7 +30,7 @@ class User:
     longitude: float
     latitude: float
     questions: list
-    recommendations: list[Restaurant]
+    recommendations: list[tuple[Restaurant, int]]
 
     def __init__(self) -> None:
         """Initialize a new Tree with the given root value and subtrees.
@@ -71,12 +70,12 @@ def get_user_info(user: User, location: str, cuisine: str, price: str, distance:
 
 
 def get_coords(ad: str) -> tuple[float, float]:
-    """Return the coordinates corresponding to the given address"""
+    """Get the coordinates of this address."""
     geolocator = Nominatim(user_agent="a")
     loc1 = geolocator.geocode(ad)
     lat = loc1.raw['lat']
     long = loc1.raw['lon']
-    return lat, long
+    return float(lat), float(long)
 
 
 # def get_user_input(questions: list[str]) -> list[str]:
@@ -161,7 +160,12 @@ class Tree:
         return self._str_indented(0).rstrip()
 
     def __repr__(self) -> str:
-        """Return a one-line string representation of this tree."""
+        """Return a one-line string representation of this tree.
+
+        >>> t = Tree(2, [Tree(4, []), Tree(5, [])])
+        >>> t
+        Tree(2, [Tree(4, []), Tree(5, [])])
+        """
         return f'Tree({self._root}, {self._subtrees})'
 
     def insert_sequence(self, items: list) -> None:
@@ -175,16 +179,24 @@ class Tree:
 
         Do nothing if items is empty.
 
-        The root of this chain (i.e. items[0]) should be added as a new subtree within this tree, as long as items[0]
-        does not already exist as a child of the current root node. That is, create a new subtree for it
-        and append it to this tree's existing list of subtrees.
-
-        If items[0] is already a child of this tree's root, instead recurse into that existing subtree rather
-        than create a new subtree with items[0]. If there are multiple occurrences of items[0] within this tree's
-        children, pick the left-most subtree with root value items[0] to recurse into.
-
         Preconditions:
             - not self.is_empty()
+
+        >>> t = Tree(111, [])
+        >>> t.insert_sequence([1, 2, 3])
+        >>> print(t)
+        111
+          1
+            2
+              3
+        >>> t.insert_sequence([1, 3, 5])
+        >>> print(t)
+        111
+          1
+            2
+              3
+            3
+              5
         """
         if not items:
             return
@@ -273,21 +285,14 @@ class Restaurant:
             return (math.acos(math.sin(lat1) * math.sin(lat2) + math.cos(lat1) * math.cos(lat2)
                               * math.cos(long2 - long1)) * 6371)
 
-    def in_price_range(self, low: int, high: int) -> Optional[bool]:
-        """Check if self is in the user's price range"""
-        if self.price_range is None:
-            return None
-        else:
-            return self.price_range[0] <= low < self.price_range[1] or self.price_range[0] < high <= self.price_range[1]
-
 
 class Event:
     """A tree node containing information about user-inputted events
     """
     name: str
     location: str
-    date: str
     time: str
+    date: str
     more_info: set[str]
 
     def __init__(self, name: str, location: str, time: str, date: str, more_info: set[str]) -> None:
@@ -297,10 +302,10 @@ class Event:
         - name != ''
         - location != ''
         """
-        self.time = time
-        self.date = date
         self.name = name
         self.location = location
+        self.time = time
+        self.date = date
         self.more_info = more_info
 
 
@@ -343,7 +348,7 @@ def get_all_cuisines() -> list:
     return list(DATA.Category.unique())
 
 
-def get_star_rating(yelp: str) -> float:
+def get_star_rating(yelp: str) -> Optional[float]:
     """get the star rating from the yelp page"""
     if yelp == '':
         return 0.0
@@ -361,35 +366,7 @@ def get_star_rating(yelp: str) -> float:
         else:
             num -= 1
             t = t[i + 1:]
-    return 0.0
-
-
-def build_decision_tree(file: str) -> Tree:
-    """Build a decision tree storing the restaurant data from the given file.
-
-    Preconditions:
-        - file is the path to a csv file in the format of the provided restaurant.csv
-    """
-    tree = Tree('', [])  # The start of a decision tree
-    with open(file, 'r', encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)  # skip the header row
-
-        for row in reader:
-            if len(row) != 9:
-                continue
-            data1 = [row[0], row[4], get_distance_from_user_og(row, (0, 0))]
-            tree.insert_sequence(data1 + [row[2]])
-
-    return tree
-
-
-def build_decision_tree2(restaurants: list[Restaurant]) -> Tree:
-    """Build a decision tree storing the restaurant data. This function will be used in run_restaurant_finder."""
-    tree = Tree('', [])
-    for rest in restaurants:
-        tree.insert_sequence([rest.price_range, rest.cuisine, rest.distance[0], rest.name])
-    return tree
+    return None
 
 
 def build_tree_w_rests(rests: list[Restaurant]) -> Tree:
@@ -399,48 +376,6 @@ def build_tree_w_rests(rests: list[Restaurant]) -> Tree:
     for i in range(len(rests)):
         tree.insert_sequence([rests[i].price_range, rests[i].cuisine, rests[i].distance[0], (rests[i], i)])
     return tree
-
-
-def get_price_range(row: list[str]) -> str:
-    """
-    FILLER
-    """
-    for item in row:
-        if item == '$11-30':
-            return item
-        if item == 'Under $10':
-            return item
-        if item == 'Above $61':
-            return item
-        if item == '$31-60':
-            return item
-    return 'No price range available'
-
-
-def get_distance_from_user_og(row: list[str], user_coordinates: tuple) -> str:
-    """
-    FILLER
-    """
-    latitude = None
-    longitude = None
-    for item in row:
-        if is_digit_or_decimal(item):
-            if latitude is None:
-                latitude = float(item)
-            else:
-                longitude = float(item)
-    if latitude is None or longitude is None:
-        return "Invalid coordinate"
-    latitude2 = math.radians(user_coordinates[0])
-    longitude2 = math.radians(user_coordinates[1])
-    distance = (math.acos(math.sin(latitude) * math.sin(latitude2) + math.cos(latitude) * math.cos(latitude2)
-                          * math.cos(longitude2 - longitude)) * 6371)
-    if distance < 1:
-        return "Under 1km"
-    elif 1 <= distance <= 5:
-        return "1-5 km"
-    else:
-        return 'Above 5km'
 
 
 def get_distance_from_user(latitude: float, longitude: float, user_coords: tuple[float, float]) -> tuple[str, float]:
@@ -461,18 +396,10 @@ def get_distance_from_user(latitude: float, longitude: float, user_coords: tuple
         return ('Above 5 km', round(distance, 4))
 
 
-def is_digit_or_decimal(s: str) -> bool:
-    """Filler"""
-    if s.startswith('-'):
-        s = s[1:]
-
-    return s.replace('.', '', 1).isdigit()
-
-
 ###############################
-def run_restaurant_finder_test(user: User) -> list[str]:  # User object must be created first
+def run_restaurant_finder2(user: User) -> list[tuple[Restaurant, int]]:  # User object must be created first
     """
-    Create a user object and find restaurants for that user based on their requirements.
+    Return a list of possible restaurants as Restaurant objects.
     """
     # if you use the same user object you get duplicate outputs...
     lst = load_data(user)
@@ -486,38 +413,26 @@ def run_restaurant_finder_test(user: User) -> list[str]:  # User object must be 
 
         load_stars(possible_rests)
         r1 = [r for r in possible_rests if r[0].star_rating is not None]
-        rests = [rest for rest in r1 if math.floor(rest[0].star_rating) == int(user.questions[3][0])]
+        rests = [rest_tup for rest_tup in r1 if math.floor(rest_tup[0].star_rating) == int(user.questions[3][0])]
     else:
         rests = possible_rests
 
-    # q = 'yes'
-    if not rests:
-        # q = 'no'
-        return []
-    elif len(rests) == 1:
-        user.recommendations.append(rests[0][0])
-        return [f'Restaurant: {rests[0][0].name}']
-    else:
-        resto_recs = []
-        restaurant = rests[0]
-        user.recommendations.append(restaurant[0])
-        resto_recs.append(f'Restaurant: {restaurant[0].name}\n')
-        return resto_recs
+    if len(rests) == 1:
+        user.recommendations.append(rests[0])
 
-    # while q == 'yes':
-    #     q = input('Would you like information about any of the recommended restaurants?')
-    #     if q == 'yes':
-    #         get_restaurant_info(user)
-    #     else:
-    #         continue
+    elif len(rests) > 1:
+        for restaurant in rests:
+            user.recommendations.append(restaurant)
+
+    return rests
 
 
-def run_restaurant_finder() -> list[str]:  # User object must be created first
+def run_restaurant_finder(user: User) -> list[str]:  # User object must be created first
     """
-    Create a user object and find restaurants for that user based on their requirements.
+    find restaurants for that user based on their requirements.
     """
-    user = User()
-    get_user_info(user, user.location, user.questions[1], user.questions[0], user.questions[2], user.questions[3])
+    # user = User()
+    # get_user_info(user, user.location, user.questions[1], user.questions[0], user.questions[2])
     # if you use the same user object you get duplicate outputs...
     lst = load_data(user)
     tree = build_tree_w_rests(lst)
@@ -550,13 +465,6 @@ def run_restaurant_finder() -> list[str]:  # User object must be created first
             recommend.append(f'Restaurant: {restaurant[0].name}\n')
         return recommend
 
-    # while q == 'yes':
-    #     q = input('Would you like information about any of the recommended restaurants?')
-    #     if q == 'yes':
-    #         get_restaurant_info(user)
-    #     else:
-    #         continue
-
 
 def load_stars(rests: list[tuple[Restaurant, int]]) -> None:
     """Helper function for run_restaurant_finder that loads star ratings into the Restaurant objects.
@@ -573,32 +481,50 @@ def load_stars(rests: list[tuple[Restaurant, int]]) -> None:
 
 
 def get_restaurant_info(user: User, restaurant: str, loc: bool, con: bool, review: bool) -> list:
-    """Display information about the restaurant recommended by run_restaurant_finder."""
+    """Display information about the restaurant recommended by run_restaurant_finder.
+
+    Preconditions:
+    - len(lst) == len(data)
+    """
+    # rest = input('Enter the name of the recommended restaurant you want more information about\n')
+    # location_info = input('Would you like location information? (Enter yes/no)\n')
+    # contact_info = input('Would you like contact information? (Enter yes/no)\n')
+    # if user.questions[3] != 'Any':
+    #     review_info = input('Would you like Yelp review information? (Enter yes/no)\n')
+    # else:
+    #     review_info = 'no'
+
+    # cuisine_category = user.questions[1]
+    # indices = data.index[data['Category'] == cuisine_category].tolist()
+    # smaller_list = lst[indices[0]:indices[len(indices) - 1] + 1]
 
     matches = []
 
     for i in user.recommendations:
-        if i.name == restaurant and i.distance[0] == user.questions[2]:
-            clean_ad = i.address.replace("\n", " ")
+        if i[0].name == restaurant and i[0].distance[0] == user.questions[2]:
+            clean_ad = i[0].address.replace("\n", " ")
             loc_info, rev_info, con_info = '', '', ''
             if loc:
-                loc_info = f'This restaurant is located {i.distance[1]} km away at {clean_ad}.'
+                loc_info = f'{i[0].name} is located {i[0].distance[1]} km away at {clean_ad}.'
             if con:
-                con_info = f"This restaurant's phone number is {i.contact[0]}, and their website is {i.contact[1]}."
+                con_info = f"{i[0].name}'s phone number is {i[0].contact[0]}, their website is {i[0].contact[1]}."
             if review:
-                rev_info = f"This restaurant's Yelp rating is {i.star_rating}."
-            matches.append([i, [loc_info, con_info, rev_info]])
-
-    note = ''
-    if len(matches) > 1 and all(x[1].contact[1] == y[1].contact[1] for x in matches for y in matches):
-        print('Note: this restaurant has multiple locations that meet your requirements.')
-    elif len(matches) > 1:
-        print('Note: there are multiple restaurants with the same name that meet your requirements.')
-    matches.append(note)
+                rating = get_info_hlpr(i)
+                rev_info = f"{i[0].name}'s Yelp rating is {rating}."
+            matches.append([loc_info, con_info, rev_info])
 
     return matches
     # if len(matches) == 0:
     #     print('The restaurant you provided is not one of your recommended restaurants.')
+
+
+def get_info_hlpr(i: tuple[Restaurant, int]) -> str:
+    """Return the necessary star rating."""
+    load_stars([i])
+    if i[0].star_rating == 0.0:
+        return 'NaN'
+    else:
+        return str(i[0].star_rating)
 
 
 def display_map_all_rests() -> None:
@@ -682,7 +608,6 @@ def display_map_recommended(u: User) -> None:
 
 # print(array)
 
-
 ###################################################################################################
 # Main block
 ###################################################################################################
@@ -701,6 +626,7 @@ if __name__ == '__main__':
     # When you are ready to check your work with python_ta, uncomment the following lines.
     # (In PyCharm, select the lines below and press Ctrl/Cmd + / to toggle comments.)
     import python_ta
+
     python_ta.check_all(config={
         'max-line-length': 120,
         'extra-imports': imports,
